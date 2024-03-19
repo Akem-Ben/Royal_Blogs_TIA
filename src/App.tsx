23506b59178bdf9ea025418647182c8c0cdefaeb
Akem-Ben <akemini.ndaobong@gmail.com>
2024-03-19 19:39:42 +0100

    corrections

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Route, Routes } from 'react-router-dom'
+import { Route, RouterProvider, Routes, createBrowserRouter } from 'react-router-dom'
 import LandingPage from './pages/LandingPage/LandingPage'
 import SignupPage from './pages/SignupPage/SignupPage'
 import LoginPage from './pages/LoginPage/LoginPage'
@@ -9,19 +9,41 @@ import "react-toastify/dist/ReactToastify.css";
 import { VerificationPage } from './pages/VerificationPage/VerificationPage'
 
 
+const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <LandingPage />,
+    // errorElement: <NotFound />,
+  },
+  {
+    path: '/signup',
+    element: <SignupPage />,
+  },
+
+  {
+    path: '/login',
+    element: <LoginPage />,
+  },
+
+
+  {
+    path: '/singlepost/:postId',
+    element: <SinglePost />,
+  },
+  {
+    path: '/verification/:token',
+    element: <VerificationPage />,
+  }
+]);
+
+
+
 function App() {
 
   return (
     <>
     <ToastContainer />
-    <Routes>
-      <Route path='/' element={<LandingPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path='/login' element={<LoginPage />} />
-      <Route path='*' element={<h1>Not Found</h1>} />
-      <Route path='/singlepost/:postId' element={<SinglePost />} />
-      <Route path="/verification/:token" element={<VerificationPage />} />
-    </Routes>
+    <RouterProvider router={routes} />
     </>
   )
 }
