961fa367ed27231a8421e2946bd017fe6d16b04e
Akem-Ben <akemini.ndaobong@gmail.com>
2024-03-19 14:19:17 +0100

    changes

--- src/__tests__/app.test.tsx
+++ src/__tests__/app.test.tsx
@@ -1,13 +1,12 @@
 import {expect, test} from 'vitest';
-import {render} from '@testing-library/react';
-import LandingPage from '../pages/LandingPage/LandingPage';
-import App from '../App';
 
 // test('LandingPage', () => {
-//   const {getByText} = render(<LandingPage />);
-//   expect(getByText('Create Post')).toBeInTheDocument();
+//   render(<LoginForm />);
+//   const check = screen.getByText(/Not Registered?/i);
+//   expect(check).toBeVisible();
 // });
 
+test('demo', () => { expect(true).toBe(true) })
 // test('Renders App', () => {
 //     render(<App />);
 //     expect(true).toBeTruthy();
